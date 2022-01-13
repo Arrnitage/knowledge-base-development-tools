d81f020d019cf893223e41a4f4afe5be9ba29ec7
an0th3r <an0ther1412@qq.com>
2022-01-13 11:48:37 +0800

    Add hocon format function

---
+++ common/hocon.py
@@ -0,0 +1,8 @@
+# TODO list to hocon format
+def list_format(list_class: list):
+    return ""
+
+
+# TODO multiline string to hocon format
+def multiline_string_format(multiline_string_class: str):
+    return ""
