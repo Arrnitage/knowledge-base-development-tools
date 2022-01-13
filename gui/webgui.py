f3774be17211c0faacc4c19eec0d90df83076705
an0th3r <an0ther1412@qq.com>
2022-01-13 19:35:24 +0800

    Finish submit function web traffic

--- gui/webgui.py
+++ gui/webgui.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, redirect, url_for, Response
+from flask import Flask, render_template, Response, request, redirect, url_for
 from .utils.form import *
 from knowledge.knowledge import Knowledge
 from common.fileutils import check_file_exist
+import os
 
 app = Flask(__name__)
+base = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 
 @app.route('/index', methods=['GET', 'POST'])
@@ -12,6 +14,7 @@ def index():
     return render_template('/index.html')
 
 
+# TODO: Need Fix(privilege has two condition string)
 @app.route('/editor', methods=['GET', 'POST'])
 def editor():
     return render_template('/editor.html',
@@ -26,23 +29,27 @@ def editor():
 @app.route('/submit', methods=['POST'])
 def submit():
     # return Response("SUCCESS")
-    file_path = ""
-    items = dict()
-    try:
-        k = Knowledge()
-        k.write(file_path)
-        return Response("SUCCESS")
-    except Exception as e:
-        return Response(str(e))
+    data = request.form.to_dict()
+    if 'location' in data:
+        location = data['location']
+        del data['location']
+    if 'k_name' in data:
+        filename = data['k_name'] + ".ttp"
+    file_path = os.path.sep.join([base, 'offensive', location, filename])
+    for key, value in data.items():
+        pass
+    return redirect(url_for('/editor'))
 
 
 @app.route('/check', methods=['POST'])
 def check():
-    file_path = ""
-    if check_file_exist(file_path):
-        return Response("SUCCESS")
+    location = request.form['location']
+    filename = request.form['filename'] + ".ttp"
+    file_path = os.path.sep.join([base, 'offensive', location, filename])
+    if not check_file_exist(file_path):
+        return Response(":) OK, You can use this file name")
     else:
-        return Response("BAD")
+        return Response(":( Already Exist, Please change the file name")
 
 
 # TODO view an offensive tech file via get request
